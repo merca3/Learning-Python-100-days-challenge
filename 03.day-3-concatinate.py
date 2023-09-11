ee744bd6006ba90172331f957a4cbd8ae85d50e8
Katrina Merca <merca3@inbox.lv>
2023-09-11 15:57:36 +0300

    add: day 3-4

---
+++ 03.day-3-concatinate.py
@@ -0,0 +1,17 @@
+food = input("Name a food > ")
+plant = input("Name a type of plant > ")
+cookingMethod = input("Name a method of cooking > ")
+burnedFoodDescription = input("What word describes burned food? > ")
+householdItem = input("Name a DYI item > ")
+
+print("MENU")
+print(
+    cookingMethod,
+    food,
+    "with",
+    burnedFoodDescription,
+    plant,
+    "on a bed of",
+    householdItem,
+    "s",
+)
---
+++ 04.day-4-story-color.py
@@ -0,0 +1,32 @@
+print(
+    """
+Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!
+"""
+)
+print()
+
+name = input("What is your name? ")
+enemyName = input("What is your worst enemy's name? ")
+superpower = input("What is your superpower? ")
+home = input("Where do you live? ")
+food = input("What is your favorite food? ")
+
+print()
+
+print(
+    "Hello",
+    name,
+    "! Your ability to",
+    superpower,
+    "will make sure you never have to look at",
+    enemyName,
+    "again. Go eat",
+    food,
+    "as you walk down the streets of",
+    home,
+    "and use",
+    superpower,
+    "\033[35m",
+    "for good and not evil!",
+    "\033[0m",
+)
